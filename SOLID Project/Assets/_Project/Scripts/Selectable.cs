8c60919a3123abc0e051a7734f1b23a920f5a123
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2024-11-14 23:56:44 -0700

    added dot product

---
+++ SOLID Project/Assets/_Project/Scripts/Selectable.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI lookPercentageLabel;
+    [SerializeField] public float LookPercentage;
+
+    void Update()
+    {
+        //lookPercentageLabel.text = LookPercentage.ToString("F3");    
+    }
+}
